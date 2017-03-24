490da25093e004abe8e67daadc8b66a1a8b60541
Pavel <pavels.voitehovics@gmail.com>
2017-03-24 11:48:26 +0100

    Some basic validation

--- handler/handler.go
+++ handler/handler.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"encoding/json"
 	"log"
+	"net/url"
 	"github.com/valyala/fasthttp"
 	"../storage"
 )
@@ -14,13 +15,20 @@ func Handler(ctx *fasthttp.RequestCtx) {}
 func CreateUrlHandler(h fasthttp.RequestHandler, s *storage.Storage, c Configuration) fasthttp.RequestHandler {
 	return fasthttp.RequestHandler(func(ctx *fasthttp.RequestCtx) {
 		if !CheckAuthentication(c, ctx) {
-			ctx.SetStatusCode(403)
+			ctx.SetStatusCode(401)
+			fmt.Fprintf(ctx, "Unauthorized")
 		} else {
 			query := ctx.URI().QueryArgs()
 			urlAddress := string(query.Peek("url"))
-			url := storage.CreateUrl(s, urlAddress)
-			fmt.Fprintf(ctx, "%s", ConvertUrlToJson(url))
-			ctx.SetContentType("application/json")
+			_, err := url.ParseRequestURI(urlAddress)
+			if err != nil {
+				ctx.SetStatusCode(500)
+				fmt.Fprintf(ctx, "Invalid URL")
+			} else {
+				url := storage.CreateUrl(s, urlAddress)
+				fmt.Fprintf(ctx, "%s", ConvertUrlToJson(url))
+				ctx.SetContentType("application/json")
+			}
 		}
 		h(ctx)
 	})
@@ -29,12 +37,17 @@ func CreateUrlHandler(h fasthttp.RequestHandler, s *storage.Storage, c Configura
 func GetUrlHandler(h fasthttp.RequestHandler, s *storage.Storage, c Configuration) fasthttp.RequestHandler {
 	return fasthttp.RequestHandler(func(ctx *fasthttp.RequestCtx) {
 		if !CheckAuthentication(c, ctx) {
-			ctx.SetStatusCode(403)
+			ctx.SetStatusCode(401)
 		} else {
 			uid := ctx.UserValue("uid").(string)
 			url := storage.GetUrl(s, uid)
-			fmt.Fprintf(ctx, "%s", ConvertUrlToJson(*url))
-			ctx.SetContentType("application/json")
+			if url.Url != "" {
+				fmt.Fprintf(ctx, "%s", ConvertUrlToJson(*url))
+				ctx.SetContentType("application/json")
+			} else {
+				ctx.SetStatusCode(404)
+				fmt.Fprintf(ctx, "Not Found")
+			}
 		}
 		h(ctx)
 	})
@@ -44,8 +57,13 @@ func RedirectHandler(h fasthttp.RequestHandler, s *storage.Storage) fasthttp.Req
 	return fasthttp.RequestHandler(func(ctx *fasthttp.RequestCtx) {
 		uid := ctx.UserValue("uid").(string)
 		url := storage.GetUrl(s, uid)
-		storage.UpdateStatistics(s, uid)
-		ctx.Redirect(url.Url, 302)
+		if url.Url != "" {
+			storage.UpdateStatistics(s, uid)
+			ctx.Redirect(url.Url, 301)
+		} else {
+			ctx.SetStatusCode(404)
+			fmt.Fprintf(ctx, "Not Found")
+		}
 		h(ctx)
 	})
 }
